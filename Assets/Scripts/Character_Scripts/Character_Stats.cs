c82d2fb54829b77c1e7a13eb5688e08931666408
Parek8 <polakpetr8878@gmail.com>
2023-10-15 23:03:36 +0200

    Some work on Hunger mechanics

--- Assets/Scripts/Character_Scripts/Character_Stats.cs
+++ Assets/Scripts/Character_Scripts/Character_Stats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,5 +12,43 @@ public class Character_Stats : MonoBehaviour
     [field: SerializeField] public float attack_distance { get; private set; } = 1.2f;
     [field: SerializeField] public LayerMask destroyable_layers { get; private set; }
     [field: SerializeField] public float attack_damage { get; private set; } = 10f;
+
+    [field: Header("Player Health Variables: ")]
     [field: SerializeField] public float food_delay { get; private set; } = 10f;
+    [field: SerializeField] public float on_hunger_hit_delay { get; private set; } = 10f;
+    [field: SerializeField] public float max_health_points { get; private set; } = 10f;
+    [field: SerializeField] public float max_food_points { get; private set; } = 10f;
+    public float current_health_points { get; private set; } = 10f;
+    public float current_food_points { get; private set; } = 10f;
+
+    private void Start()
+    {
+        current_health_points = max_health_points;
+        current_food_points = max_food_points;
+    }
+
+    public void Reduce_Health(float reduce_hp)
+    {
+        current_health_points -= reduce_hp;
+        if(current_food_points <= 0)
+        {
+            Debug.Log("You've died!");
+        }
+    }
+
+    public void Reduce_Food(float reduce_food)
+    {
+        current_food_points -= reduce_food;
+        if (current_food_points <= 0)
+            StartCoroutine(Damage_On_Hunger());
+    }
+
+    private IEnumerator Damage_On_Hunger()
+    {
+        while(current_food_points <= 0) 
+        {
+            Reduce_Health(1.0f);
+            yield return new WaitForSeconds(on_hunger_hit_delay);
+        }
+    }
 }
--- Assets/Scripts/Character_Scripts/Player_Health.cs
+++ Assets/Scripts/Character_Scripts/Player_Health.cs
@@ -5,13 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(Character_Stats))]
 public class Player_Health : MonoBehaviour
 {
+    //[field: SerializeField] 
     Character_Stats stats;
+    float current_food_delay = 20;
     private void Start()
     {
-        stats = GetComponent<Character_Stats>();    
+        stats = GetComponent<Character_Stats>();
+        current_food_delay = stats.food_delay;
+        StartCoroutine(Hunger());
     }
-    private void Update()
+    private IEnumerator Hunger()
     {
-        
+        while(true)
+        {
+            Reduce_Hunger();
+            yield return new WaitForSeconds(current_food_delay);
+        }
+    }
+
+    private void Reduce_Hunger()
+    {
+        stats.Reduce_Food(1.0f);
+    }
+    public void Hit()
+    {
+
+    }
+    public void Update_UI()
+    {
+        int hp = (int)stats.current_health_points;
+        int hunger = (int)stats.current_food_points;
+
+
+
     }
 }

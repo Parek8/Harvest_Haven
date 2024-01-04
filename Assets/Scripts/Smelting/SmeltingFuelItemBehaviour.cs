54bfcb086c15d34197f71ce1fb3faaf5c5adec3f
Párek8 <polakpetr8878@gmail.com>
2024-01-04 16:27:21 +0100

    Feat: Fuel Slot begin

---
+++ Assets/Scripts/Smelting/SmeltingFuelItemBehaviour.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmeltingFuelItemBehaviour : UI_Behaviour
+{
+    [field: SerializeField] UI_Behaviour ResultScreen;
+    SmeltingSLot _assignedSlot;
+
+    public void AssignSlot(CraftingSlot _slot)
+    {
+        if (_slot != null)
+            _assignedSlot = _slot;
+    }
+
+    public void AssignItem(Item _item)
+    {
+        if (_item != null)
+            _assignedSlot.AssignItem(_item);
+    }
+    public override void _Hide()
+    {
+        base._Hide();
+        ResultScreen.Show();
+    }
+    public override void _Show()
+    {
+        base._Show();
+        ResultScreen.Hide();
+    }
+}

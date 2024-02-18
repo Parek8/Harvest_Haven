cb30364c6d310d8c0ea1bce0292f2866ddd6fc89
Párek8 <petrpolak11@zak.spsul.cz>
2024-02-18 22:08:25 +0100

    Feat: New Models

--- Assets/Scripts/ProceduralGen/DungeonGenerator.cs
+++ Assets/Scripts/ProceduralGen/DungeonGenerator.cs
@@ -1,38 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class DungeonGenerator : MonoBehaviour
 {
-
+    [System.Serializable]
     public class Cell
     {
         public bool HasBeenVisited = false;
         public bool[] CellStatus = new bool[4];
+        public bool IsNull = true;
+        public Rule CellRule;
+    }
+    [System.Serializable]
+    public class Rule
+    {
+        public Vector2 Size;
+        public GameObject RoomPrefab;
     }
 
-
-    public Vector2 Size;
-    public int StartPosition = 0;
-    public GameObject RoomPrefab;
+    public List<Rule> RoomPrefabs;
     public Vector2 RoomOffset;
+    public Vector2 BaseSize = new();
+    public Vector2 DungeonSize = new();
+    public int StartPosition = 0;
 
-    List<Cell> board;
+    public List<Cell> board;
+    public Rule FinishRule;
+    public Rule StartRule;
+
+    bool hasStart = false;
+    bool hasFinish = false;
     private void Start() 
     {
         GenerateMaze();
     }
     void GenerateDungeon()
     {
-        for (int i = 0; i < Size.x; i++)
+        for (int i = 0; i < DungeonSize.x; i++)
         {
-            for (int j = 0; j < Size.y; j++)
+            for (int j = 0; j < DungeonSize.y; j++)
             {
-                if (board[Mathf.FloorToInt(i+j*Size.x)].HasBeenVisited)
+                if (board[Mathf.FloorToInt(i+j* DungeonSize.x)].HasBeenVisited && !board[Mathf.FloorToInt(i + j * DungeonSize.x)].IsNull)
                 {
-                    RoomBehaviour newRoom = Instantiate(RoomPrefab, new Vector3(i*RoomOffset.x, 0, -j*RoomOffset.y), Quaternion.identity).GetComponent<RoomBehaviour>();
-                    newRoom.UpdateRoom(board[Mathf.FloorToInt(i+j*Size.x)].CellStatus);
+                    if (!hasFinish && Random.Range(0f, 1f) > 0.84f)
+                    {
+                        RoomBehaviour newFinish = Instantiate(FinishRule.RoomPrefab, new Vector3(i * RoomOffset.x, 0, -j * RoomOffset.y), Quaternion.identity).GetComponent<RoomBehaviour>();
+                        newFinish.UpdateRoom(board[Mathf.FloorToInt(i + j * DungeonSize.x)].CellStatus);
+
+                        newFinish.name = $"FINISH";
+                        hasFinish = true;
+                    }
+                    if (!hasStart && Random.Range(0f, 1f) > 0.84f)
+                    {
+                        RoomBehaviour newStart = Instantiate(StartRule.RoomPrefab, new Vector3(i * RoomOffset.x, 0, -j * RoomOffset.y), Quaternion.identity).GetComponent<RoomBehaviour>();
+                        newStart.UpdateRoom(board[Mathf.FloorToInt(i + j * DungeonSize.x)].CellStatus);
+
+                        newStart.name = $"START";
+                        hasStart = true;
+                    }
+                    RoomBehaviour newRoom = Instantiate(board[Mathf.FloorToInt(i + j * DungeonSize.x)].CellRule.RoomPrefab, new Vector3(i*RoomOffset.x, 0, -j*RoomOffset.y), Quaternion.identity).GetComponent<RoomBehaviour>();
+                    newRoom.UpdateRoom(board[Mathf.FloorToInt(i+j* DungeonSize.x)].CellStatus);
 
                     newRoom.name = $"Cell[{i},{j}]";
                 }
@@ -44,9 +77,9 @@ public class DungeonGenerator : MonoBehaviour
     {
         board = new List<Cell>();
 
-        for (int i = 0; i < Size.x; i++)
+        for (int i = 0; i < DungeonSize.x; i++)
         {
-            for (int j = 0; j < Size.y; j++)
+            for (int j = 0; j < DungeonSize.y; j++)
             {
                 board.Add(new Cell());
             }
@@ -58,8 +91,9 @@ public class DungeonGenerator : MonoBehaviour
 
         int k = 0;
 
-        while (k < 100)
+        while (k < 2000)
         {
+
             if (currentCell == board.Count)
                 break;
                 
@@ -67,6 +101,7 @@ public class DungeonGenerator : MonoBehaviour
             board[currentCell].HasBeenVisited = true;
 
             List<int> cellNeighbors = FindNeighbors(currentCell);
+            Rule randomRule = RoomPrefabs[Random.Range(0, RoomPrefabs.Count)];
 
             if (cellNeighbors.Count == 0)
             {
@@ -81,38 +116,71 @@ public class DungeonGenerator : MonoBehaviour
 
                 int newCell = cellNeighbors[Random.Range(0, cellNeighbors.Count)];
 
-                if (newCell > currentCell)
+                int CellX = (int)(randomRule.Size.x / BaseSize.x);
+                int CellY = (int)(randomRule.Size.y / BaseSize.y);
+                if (CellX > 1 || CellY > 1)
                 {
-                    if (newCell - 1 == currentCell)
+                    for (int x = 0; x < (CellX*CellY); x++)
                     {
-                        // RIGHT
-                        board[currentCell].CellStatus[3] = true;
-                        currentCell = newCell;
-                        board[currentCell].CellStatus[2] =  true;
+                        board[currentCell + x].HasBeenVisited = true;
+                        board[currentCell + x].IsNull = true;
+                        Debug.Log(currentCell+x);
                     }
-                    else
-                    {
-                        // DOWN
-                        board[currentCell].CellStatus[1] = true;
-                        currentCell = newCell;
-                        board[currentCell].CellStatus[0] =  true;
-                    }
+                    board[currentCell].HasBeenVisited = true;
+                    board[currentCell].IsNull = false;
+                    board[currentCell].CellRule = randomRule;
                 }
                 else
                 {
-                    if (newCell + 1 == currentCell)
+                    if (newCell > currentCell)
                     {
-                        // LEFT
-                        board[currentCell].CellStatus[2] = true;
-                        currentCell = newCell;
-                        board[currentCell].CellStatus[3] =  true;
+                        if (newCell - 1 == currentCell)
+                        {
+                            // RIGHT
+                            board[currentCell].CellStatus[3] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = randomRule;
+                            currentCell = newCell;
+                            board[currentCell].CellStatus[2] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = RoomPrefabs[Random.Range(0, RoomPrefabs.Count - 1)];
+                        }
+                        else
+                        {
+                            // DOWN
+                            board[currentCell].CellStatus[1] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = randomRule;
+                            currentCell = newCell;
+                            board[currentCell].CellStatus[0] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = RoomPrefabs[Random.Range(0, RoomPrefabs.Count - 1)];
+                        }
                     }
                     else
                     {
-                        // UP
-                        board[currentCell].CellStatus[0] = true;
-                        currentCell = newCell;
-                        board[currentCell].CellStatus[1] =  true;
+                        if (newCell + 1 == currentCell)
+                        {
+                            // LEFT
+                            board[currentCell].CellStatus[2] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = randomRule;
+                            currentCell = newCell;
+                            board[currentCell].CellStatus[3] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = RoomPrefabs[Random.Range(0, RoomPrefabs.Count - 1)];
+                        }
+                        else
+                        {
+                            // UP
+                            board[currentCell].CellStatus[0] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = randomRule;
+                            currentCell = newCell;
+                            board[currentCell].CellStatus[1] = true;
+                            board[currentCell].IsNull = false;
+                            board[currentCell].CellRule = RoomPrefabs[Random.Range(0, RoomPrefabs.Count - 1)];
+                        }
                     }
                 }
             }
@@ -125,14 +193,14 @@ public class DungeonGenerator : MonoBehaviour
     {
         List<int> neighbors = new List<int>();
 
-        if (currentCell - Size.x >= 0 && !board[Mathf.FloorToInt(currentCell - Size.x)].HasBeenVisited)
-            neighbors.Add(Mathf.FloorToInt(currentCell - Size.x));
-        if (currentCell + Size.x < board.Count && !board[Mathf.FloorToInt(currentCell + Size.x)].HasBeenVisited)
-            neighbors.Add(Mathf.FloorToInt(currentCell + Size.x));
+        if (currentCell - DungeonSize.x >= 0 && !board[Mathf.FloorToInt(currentCell - DungeonSize.x)].HasBeenVisited)
+            neighbors.Add(Mathf.FloorToInt(currentCell - DungeonSize.x));
+        if (currentCell + DungeonSize.x < board.Count && !board[Mathf.FloorToInt(currentCell + DungeonSize.x)].HasBeenVisited)
+            neighbors.Add(Mathf.FloorToInt(currentCell + DungeonSize.x));
 
-        if ((currentCell+1) % Size.x != 0 && !board[Mathf.FloorToInt(currentCell + 1)].HasBeenVisited)
+        if ((currentCell+1) % DungeonSize.x != 0 && !board[Mathf.FloorToInt(currentCell + 1)].HasBeenVisited)
             neighbors.Add(Mathf.FloorToInt(currentCell + 1));
-        if (currentCell % Size.x != 0 && !board[Mathf.FloorToInt(currentCell - 1)].HasBeenVisited)
+        if (currentCell % DungeonSize.x != 0 && !board[Mathf.FloorToInt(currentCell - 1)].HasBeenVisited)
             neighbors.Add(Mathf.FloorToInt(currentCell - 1));
         
         return neighbors;

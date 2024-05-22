ea7c6722139ae571dc89f2d5a8e87f11b1e04d1b
Párek8 <petrpolak11@zak.spsul.cz>
2024-05-22 17:04:37 +0200

    ¨Fix: CraftingCanvas

--- Assets/Scripts/Crafting/CraftingRecipe.cs
+++ Assets/Scripts/Crafting/CraftingRecipe.cs
@@ -16,52 +16,52 @@ internal class CraftingRecipe : ScriptableObject
 
         if (_compared[0] != Items[0])
         {
-            Debug.Log("1 Item is wrong!");
+            //Debug.Log("1 Item is wrong!");
             return false;
         }
         else if (_compared[1] != Items[1])
         {
-            Debug.Log("2 Item is wrong!");
+            //Debug.Log("2 Item is wrong!");
             return false;
         }
         else if (_compared[2] != Items[2])
         {
-            Debug.Log("3 Item is wrong!");
-            return false;
-        }
-        else if (_compared[3] != Items[3])
-        {
-            Debug.Log("4 Item is wrong!");
+            //Debug.Log("3 Item is wrong!"); 
+            return false;                  
+        }                                  
+        else if (_compared[3] != Items[3]) 
+        {                                  
+            //Debug.Log("4 Item is wrong!"); 
             return false;
         }
         else if (_compared[4] != Items[4])
         {
-            Debug.Log("5 Item is wrong!");
+            //Debug.Log("5 Item is wrong!");
             return false;
         }
         else if (_compared[5] != Items[5])
         {
-            Debug.Log("6 Item is wrong!");
+            //Debug.Log("6 Item is wrong!");
             return false;
         }
         else if (_compared[6] != Items[6])
         {
-            Debug.Log("7 Item is wrong!");
+            //Debug.Log("7 Item is wrong!");
             return false;
         }
         else if (_compared[7] != Items[7])
         {
-            Debug.Log("8 Item is wrong!");
+            //Debug.Log("8 Item is wrong!");
             return false;
         }
         else if (_compared[8] != Items[8])
         {
-            Debug.Log("9 Item is wrong!");
+            //Debug.Log("9 Item is wrong!");
             return false;
         }
         else
         {
-            Debug.Log("Alles Gut");
+            //Debug.Log("Alles Gut");
             return true;
         }
     }

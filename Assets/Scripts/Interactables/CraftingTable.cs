b28f2bfbd5cd25cd744b358b0793c0ab7a583ec4
Párek8 <petrpolak11@zak.spsul.cz>
2024-05-23 13:14:56 +0200

    Fix: Post-Processing

--- Assets/Scripts/Interactables/CraftingTable.cs
+++ Assets/Scripts/Interactables/CraftingTable.cs
@@ -8,7 +8,6 @@ internal class CraftingTable : Interactable
     internal override void Interact()
     {
         _craftingScreen.Show();
-        Debug.Log("asd");
         GameManager.game_manager.Cursor_Needed(CursorLockMode.None);
     }
 }

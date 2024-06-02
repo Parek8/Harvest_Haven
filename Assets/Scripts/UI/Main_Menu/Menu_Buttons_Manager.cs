c3997be86d80fcdf24a6a9bebdc0319a6936dcd0
Párek8 <petrpolak11@zak.spsul.cz>
2024-06-02 19:21:17 +0200

    Rework: MainMenu

--- Assets/Scripts/UI/Main_Menu/Menu_Buttons_Manager.cs
+++ Assets/Scripts/UI/Main_Menu/Menu_Buttons_Manager.cs
@@ -1,9 +1,30 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 internal class Menu_Buttons_Manager : MonoBehaviour
 {
+    [field: SerializeField] Image Curtain;
+    [field: SerializeField] float OpeningTime;
+
+    private float m_CurrentTime = 0;
+    private void Start()
+    {
+        StartCoroutine(OpenCurtain());
+    }
+
+    IEnumerator OpenCurtain()
+    {
+        while (m_CurrentTime < OpeningTime)
+        {
+            m_CurrentTime += Time.deltaTime;
+            Curtain.color = Color.Lerp(new Color(0, 0, 0, 1), new Color(0, 0, 0, 0), m_CurrentTime/OpeningTime);
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
     public void Play()
     {
         // load world mechanics yet to be implemented
